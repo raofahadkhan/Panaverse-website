8be7b6b509e7dd4e05edb9e4c4293e3efc16acef
RAO FAHAD <raofahad046@gmail.com>
2023-03-01 00:13:22 +0500

    added about.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-'use client'
+"use client";
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import React from "react";
 
 function Home() {
   return (
-    <Hero/>
-  )
+    <>
+      <Hero />
+      <About />
+    </>
+  );
 }
 
 export default Home;
